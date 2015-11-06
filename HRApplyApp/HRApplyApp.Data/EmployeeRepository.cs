16827049dbdc46c7e942aa6dbb883cc6ef55cf58
RoccoRiccardi <Rocco.Riccardi.42@gmail.com>
2015-11-05 19:32:19 -0500

    added Time Controller, made a simple index page where employees are being pulled from SWCCorp DB using Dapper

---
+++ HRApplyApp/HRApplyApp.Data/EmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using HrApplyApp.Models;
+using HRApplyApp.Data.Config;
+
+namespace HRApplyApp.Data
+{
+    public class EmployeeRepository
+    {
+        public List<Employee> GetAll()
+        {
+            using (SqlConnection cn = new SqlConnection(Settings.ConnectionString))
+            {
+                var employees = cn.Query<Employee>("SELECT * FROM Employee").ToList();
+
+                return employees;
+            }
+        } 
+    }
+}
---
+++ HRApplyApp/HRApplyApp.UI/Controllers/TimeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HRApplyApp.Data;
+
+namespace HRApplyApp.UI.Controllers
+{
+    public class TimeController : Controller
+    {
+        // GET: Time
+        public ActionResult Index()
+        {
+            var repo = new EmployeeRepository();
+            var employees = repo.GetAll();
+            return View(employees);
+        }
+    }
+}
